9cb68d192be433b8de38b4ee4a63cacd3bf99e52
TangJiCong <2201449931@qq.com>
2020-04-17 22:04:50 +0800

    delate

--- NextJS-React-EggJS-blog/pages/_app.js
+++
@@ -1,9 +0,0 @@
-
-import App ,{ Container } from 'next/app'
-
-import 'antd/dist/antd.css'
-import '../public/style/pages/comm.css'
-
-//import test from  '../static/js/shubiao.js'
-
-export default App
